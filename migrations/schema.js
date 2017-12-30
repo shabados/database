7b7cb42aba995e94cca64c6e34f68b6a80953d18
Harjot Singh <harjot@harkul.com>
2017-12-30 13:06:25 +0000

    create quick schema

---
+++ migrations/schema.js
@@ -0,0 +1,48 @@
+/**
+ * Creates schema
+ */
+exports.up = knex => Promise.all( [
+  knex.schema.createTable( 'raags', table => {
+    table.increments( 'id' ).primary()
+    table.text( 'name' ).notNullable()
+  } ),
+
+  knex.schema.createTable( 'sources', table => {
+    table.increments( 'id' ).primary()
+    table.text( 'name' ).notNullable()
+  } ),
+
+  knex.schema.createTable( 'writers', table => {
+    table.increments( 'id' ).primary()
+    table.text( 'name' ).notNullable()
+  } ),
+
+  knex.schema.createTable( 'shabads', table => {
+    table.increments( 'id' ).primary()
+    table.integer( 'raag_id' ).references( 'id' ).inTable( 'raags' )
+    table.integer( 'source_id' ).references( 'id' ).inTable( 'sources' )
+    table.integer( 'writer_id' ).references( 'id' ).inTable( 'writers' )
+  } ),
+
+  knex.schema.createTable( 'lines', table => {
+    table.increments( 'id' ).primary()
+    table.integer( 'ang' ).notNullable()
+    table.integer( 'shabad_id' ).references( 'id' ).inTable( 'shabads' ).notNullable()
+    table.text( 'gurmukhi' ).notNullable()
+    table.text( 'english' )
+    table.text( 'punjabi' )
+    table.text( 'transliteration' )
+    table.text( 'pronounciation' )
+  } ),
+] )
+
+/**
+ * Drops schema
+ */
+exports.down = knex => Promise.all( [
+  knex.schema.dropTable( 'raags' ),
+  knex.schema.dropTable( 'writers' ),
+  knex.schema.dropTable( 'sources' ),
+  knex.schema.dropTable( 'shabads' ),
+  knex.schema.dropTable( 'lines' ),
+] )
