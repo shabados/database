87efd411f48b4197a5f0784fcd619c590501dde6
Harjot Singh <harjot@harkul.com>
2018-01-02 16:49:25 +0000

    seeds: refactor functions into utils file, use shabads table instead of gurmukhi table

--- seeds/seed.js
+++ seeds/seed.js
@@ -4,7 +4,8 @@
  */
 
 const { readdirSync } = require( 'fs' )
-const { basename } = require( 'path' )
+
+const { stripExtension, generateFirstLetters } = require( '../lib/utils' )
 
 const raags = require( './raags' )
 const sources = require( './sources' )
@@ -14,15 +15,15 @@ const lineTypes = require( './line_types' )
 const SHABADS_DIR = 'shabads'
 const SOURCES_DIR = 'sources'
 
-const stripExtension = name => basename( name, '.json' )
 
 // Map each of sources to a table name
 const tables = {
   raags,
   sources,
   writers,
-  line_types: lineTypes
+  line_types: lineTypes,
 }
+
 
 exports.seed = knex => knex.transaction( async trx => {
   // Transform each of the files and then insert into DB
@@ -30,32 +31,34 @@ exports.seed = knex => knex.transaction( async trx => {
     // Transform each array item into { id, name } objects
     knex
       .batchInsert( tableName, data.map( ( name, index ) => ( { id: index + 1, name } ) ) )
-      .transacting( trx )
-  ) )
+      .transacting( trx ) ) )
 
   // Insert all the shabads
   const shabads = readdirSync( `seeds/${SHABADS_DIR}` )
+    .sort( ( s1, s2 ) => sources.indexOf( s1 ) - sources.indexOf( s2 ) )
     .reduce( ( data, source ) => [
       ...data,
       ...readdirSync( `seeds/${SHABADS_DIR}/${source}` )
         .map( stripExtension )
         .map( writer => [ writer, `./${SHABADS_DIR}/${source}/${writer}` ] )
+        // eslint-disable-next-line
         .map( ( [ writer, path ] ) => [ writer, require( path ) ] )
         .map( ( [ writer, shabads ] ) => shabads.map( shabad => ( {
           ...shabad,
           source_id: sources.indexOf( source ) + 1,
-          writer_id: writers.indexOf( writer ) + 1
-        } ) ) )
+          writer_id: writers.indexOf( writer ) + 1,
+        } ) ) ),
     ], [] )
     .reduce( ( allData, data ) => [ ...allData, ...data ], [] )
 
-  await Promise.all( shabads.map( shabad => knex( 'shabads' )
-    .insert( shabad )
-    .transacting( trx ) ) )
+  await Promise.all( shabads
+    .map( shabad => knex( 'shabads' )
+      .insert( shabad )
+      .transacting( trx ) ) )
 
   // Insert all the lines
-  // TODO: insert in order of sources.json to start SGGS from line 1 here
   const lines = readdirSync( `seeds/${SOURCES_DIR}` )
+    .sort( ( s1, s2 ) => sources.indexOf( s1 ) - sources.indexOf( s2 ) )
     .reduce( ( data, source ) => [
       ...data,
       ...readdirSync( `seeds/${SOURCES_DIR}/${source}` )
@@ -64,18 +67,19 @@ exports.seed = knex => knex.transaction( async trx => {
           ...readdirSync( `seeds/${SOURCES_DIR}/${source}/${batch}` )
             .map( stripExtension )
             .map( ang => [ ang, `./${SOURCES_DIR}/${source}/${batch}/${ang}` ] )
+            // eslint-disable-next-line
             .map( ( [ ang, path ] ) => [ ang, require( path ) ] )
             .map( ( [ ang, lines ] ) => lines.map( line => ( {
               ...line,
-              ang
-            } ) ) )
-        ], [] )
+              ang,
+              first_letters: generateFirstLetters( source, line ),
+            } ) ) ),
+        ], [] ),
     ], [] )
     .reduce( ( allData, data ) => [ ...allData, ...data ], [] )
 
-  await Promise.all( lines.map( lines => knex( 'lines' )
-    .insert( lines )
-    .transacting( trx )
-  ) )
-
+  await Promise.all( lines
+    .map( lines => knex( 'lines' )
+      .insert( lines )
+      .transacting( trx ) ) )
 } )
