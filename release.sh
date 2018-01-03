a893702c3e47c54f0f5daf6db06ab62f7c0061fe
Harjot Singh <harjot@harkul.com>
2018-01-03 23:44:48 +0000

    ci: modify script to detect major, minor, and patch releases. MINOR changes

--- release.sh
+++ release.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env bash
+
+log_messages=$(git log origin/master...HEAD --format=%s)
+
+if [[ ${log_messages} =~ \bMAJOR\b ]]; then
+    echo "Major Release"
+#    release-it major -n --no-npm.publish --github.release --github.assets=build/database.sqlite
+elif [[ ${log_messages} =~ \bMINOR\b ]]; then
+    echo "Minor Release"
+#    release-it major -n --no-npm.publish --github.release --github.assets=build/database.sqlite
+else
+    echo "Patch Release"
+#    release-it patch -n --no-npm.publish --github.release --github.assets=build/database.sqlite
+fi
