8839fd5519cb1cc464d0f442c8a183eb79058db0
Harjot Singh <harjot@harkul.com>
2018-01-04 00:18:29 +0000

    Update release.sh

--- release.sh
+++ release.sh
@@ -4,6 +4,8 @@ log_messages=$(git log origin/master...HEAD --format=%s)
 major_re=\\bMAJOR\\b
 minor_re=\\bMINOR\\b
 
+echo ${log_messages}
+
 if [[ ${log_messages} =~ ${major_re}  ]]; then
     echo "Major Release"
     release-it major -n --github.release --github.assets=build/database.sqlite
