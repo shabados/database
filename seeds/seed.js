e62bceda4d2c33a7bb87d15e535c331d37a6ad23
Harjot Singh <harjot@harkul.com>
2018-01-04 04:55:02 +0000

    seeds: insert bani files into DB

--- seeds/seed.js
+++ seeds/seed.js
@@ -24,28 +24,60 @@ const tables = {
   line_types: lineTypes,
 }
 
-
-exports.seed = knex => knex.transaction( async trx => {
-  // Transform each of the independent json files and then insert into DB
-  await Promise.all( Object.entries( tables ).map( ( [ tableName, data ] ) =>
-    // Transform each array item into { id, name } objects
-    knex
+// Transform each of the independent json files and then insert into DB
+const insertTables = ( knex, trx ) => Promise.all( (
+  Object.entries( tables )
+    .map( ( [ tableName, data ] ) => knex
       .batchInsert( tableName, data.map( ( name, index ) => ( { id: index + 1, name } ) ) )
-      .transacting( trx ) ) )
+      .transacting( trx ) )
+) )
+
+// Inserts all the banis into the bani and bani_lines tables
+const insertBanis = async ( knex, trx ) => {
+  // Get the bani names
+  const banis = readdirSync( `seeds/${BANIS_DIR}` ).map( stripExtension )
 
   // Insert all the banis
-  const banis = readdirSync(`seeds/${BANIS_DIR}`)
+  for ( const name of banis ) {
+    const [ baniId ] = await knex( 'banis' )
+      .insert( { name } )
+      .returning( '*' )
+      .transacting( trx )
 
-  // Insert all the shabads
+    // eslint-disable-next-line
+    const baniLines = require( `./${BANIS_DIR}/${name}` )
+    // Generate line_ids between start_line and end_line
+      .map( ( { start_line: startLine, end_line: endLine } ) => Array.from(
+        new Array( ( endLine - startLine ) + 1 ),
+        ( val, index ) => index + startLine,
+      ) )
+      // Group into their lines, and flatten them
+      .reduce( ( lines, groups, index ) => [
+        ...lines,
+        ...groups.map( lineId => ( {
+          bani_id: baniId,
+          line_id: lineId,
+          line_group: index + 1,
+        } ) ),
+      ], [] )
+
+    // Insert into the database
+    await Promise.all( baniLines.map( baniLine => (
+      knex( 'bani_lines' ).insert( baniLine ).transacting( trx )
+    ) ) )
+  }
+}
+
+// Retrieves, flattens, and inserts the shabads from the various folders
+const insertShabads = async ( knex, trx ) => {
   const shabads = readdirSync( `seeds/${SHABADS_DIR}` )
     .sort( ( s1, s2 ) => sources.indexOf( s1 ) - sources.indexOf( s2 ) )
     .reduce( ( data, source ) => [
       ...data,
       ...readdirSync( `seeds/${SHABADS_DIR}/${source}` )
         .map( stripExtension )
-        .map( writer => [ writer, `./${SHABADS_DIR}/${source}/${writer}` ] )
         // eslint-disable-next-line
-        .map( ( [ writer, path ] ) => [ writer, require( path ) ] )
+        .map( writer => [ writer, require( `./${SHABADS_DIR}/${source}/${writer}` ) ] )
         .map( ( [ writer, shabads ] ) => shabads.map( shabad => ( {
           ...shabad,
           source_id: sources.indexOf( source ) + 1,
@@ -54,10 +86,14 @@ exports.seed = knex => knex.transaction( async trx => {
     ], [] )
     .reduce( ( allData, data ) => [ ...allData, ...data ], [] )
 
+  // Insert all the shabads
   await Promise.all( shabads.map( shabad => (
     knex( 'shabads' ).insert( shabad ).transacting( trx )
   ) ) )
+}
 
+// Retrieves, flattens, and inserts all the lines from the nested folders
+const insertLines = async ( knex, trx ) => {
   // Insert all the lines
   const lines = readdirSync( `seeds/${SOURCES_DIR}` )
     .sort( ( s1, s2 ) => sources.indexOf( s1 ) - sources.indexOf( s2 ) )
@@ -70,9 +106,8 @@ exports.seed = knex => knex.transaction( async trx => {
           ...readdirSync( `seeds/${SOURCES_DIR}/${source}/${batch}` )
             .map( stripExtension )
             .sort( ( a1, a2 ) => a1 - a2 )
-            .map( ang => [ ang, `./${SOURCES_DIR}/${source}/${batch}/${ang}` ] )
             // eslint-disable-next-line
-            .map( ( [ ang, path ] ) => [ ang, require( path ) ] )
+            .map( ang => [ ang, require( `./${SOURCES_DIR}/${source}/${batch}/${ang}` ) ] )
             .map( ( [ ang, lines ] ) => lines.map( line => ( {
               ...line,
               ang,
@@ -86,4 +121,11 @@ exports.seed = knex => knex.transaction( async trx => {
   for ( const line of lines ) {
     await knex( 'lines' ).insert( line ).transacting( trx )
   }
+}
+
+exports.seed = knex => knex.transaction( async trx => {
+  await insertTables( knex, trx )
+  await insertBanis( knex, trx )
+  await insertShabads( knex, trx )
+  await insertLines( knex, trx )
 } )
