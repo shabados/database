0a609e71c708dd0a8d922c41c385c50422f2499f
Harjot Singh <harjot@harkul.com>
2018-01-04 03:50:59 +0000

    seeds: generate id

--- seeds/seed.js
+++ seeds/seed.js
@@ -7,7 +7,6 @@ const { readdirSync } = require( 'fs' )
 
 const { stripExtension, generateFirstLetters } = require( '../lib/utils' )
 
-const banis = require( './banis' )
 const raags = require( './raags' )
 const sources = require( './sources' )
 const writers = require( './writers' )
@@ -15,25 +14,27 @@ const lineTypes = require( './line_types' )
 
 const SHABADS_DIR = 'shabads'
 const SOURCES_DIR = 'sources'
-
+const BANIS_DIR = 'banis'
 
 // Map each of sources to a table name
 const tables = {
   raags,
   sources,
   writers,
-  banis,
   line_types: lineTypes,
 }
 
 
 exports.seed = knex => knex.transaction( async trx => {
-  // Transform each of the files and then insert into DB
+  // Transform each of the independent json files and then insert into DB
   await Promise.all( Object.entries( tables ).map( ( [ tableName, data ] ) =>
     // Transform each array item into { id, name } objects
     knex
       .batchInsert( tableName, data.map( ( name, index ) => ( { id: index + 1, name } ) ) )
       .transacting( trx ) ) )
+
+  // Insert all the banis
+  const banis = readdirSync(`seeds/${BANIS_DIR}`)
 
   // Insert all the shabads
   const shabads = readdirSync( `seeds/${SHABADS_DIR}` )
@@ -81,31 +82,8 @@ exports.seed = knex => knex.transaction( async trx => {
     ], [] )
     .reduce( ( allData, data ) => [ ...allData, ...data ], [] )
 
-  // Pluck banis from lines and generate BaniLines from ranges
-  const { baniLines } = lines.reduce( (
-    { currentBanis, baniLines },
-    { start_banis: startBanis = [], end_banis: endBanis = [] },
-    index,
-  ) => {
-    startBanis.forEach( id => currentBanis.add( id ) )
-    // Assuming that the lineId in SQLite === index+1 in array here
-    const lines = [
-      ...baniLines,
-      ...[ ...currentBanis ].map( id => ( { bani_id: id, line_id: index + 1 } ) ),
-    ]
-    endBanis.forEach( id => currentBanis.delete( id ) )
-    return { currentBanis, baniLines: lines }
-  }, { currentBanis: new Set(), baniLines: [] } )
-
   // Use for-of instead to insert lines sequentially and preserve order
   for ( const line of lines ) {
-    delete line.start_banis
-    delete line.end_banis
     await knex( 'lines' ).insert( line ).transacting( trx )
   }
-
-  // Insert the bani lines
-  await Promise.all( baniLines.map( baniLine => (
-    knex( 'bani_lines' ).insert( baniLine ).transacting( trx )
-  ) ) )
 } )
