b027a280b7ce36c2f2706c10c2e09ae7aa292ba1
Harjot Singh <harjot@harkul.com>
2018-03-23 16:38:47 +0000

    ci: update release script to prevent release [CI SKIP]

--- release.sh
+++ release.sh
@@ -7,6 +7,7 @@ set -o pipefail
 log_messages=$(git log -1 --format=%s)
 major_re=\\bMAJOR\\b
 minor_re=\\bMINOR\\b
+no_re=\\bNO-RELEASE\\b
 
 echo ${log_messages}
 
@@ -18,6 +19,8 @@ if [[ ${log_messages} =~ ${major_re}  ]]; then
 elif [[ ${log_messages} =~ ${minor_re} ]]; then
     echo "Minor Release"
     release-it minor ${args}
+elif [[ ${log_messages} =~ ${minor_re} ]]; then
+    echo "Not releasing"
 else
     echo "Patch Release"
     release-it patch ${args}
