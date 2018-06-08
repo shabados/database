2848e264e2d0caf385c5d32961351fc276dffa10
Harjot Singh <harjot@harkul.com>
2018-06-08 20:43:55 +0100

    Reshuffle folder structure

--- knexfile.js
+++ knexfile.js
@@ -2,6 +2,7 @@ const config = {
   client: 'sqlite3',
   connection: { filename: `${__dirname}/build/database.sqlite` },
   useNullAsDefault: true,
+  migrations: { directory: './lib/migrations' },
 }
 
 module.exports = config
