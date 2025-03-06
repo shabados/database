bf109bd8f97804bbc7fab3b7c7a0644c40c02a0a
Harjot Singh <harjot@harkul.com>
2025-03-06 19:22:33 +0000

    build: add commitlint support

---
+++ commitlint.config.ts
@@ -0,0 +1,14 @@
+import conventional from '@commitlint/config-conventional'
+
+import { RuleConfigSeverity, type UserConfig } from '@commitlint/types'
+
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      RuleConfigSeverity.Error,
+      'always',
+      [...conventional.rules['type-enum'][2], 'data'],
+    ],
+  },
+} satisfies UserConfig
