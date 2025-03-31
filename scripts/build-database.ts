ed0f88461291c677575de8f5097af0f6944f3d86
Harjot Singh <harjot@harkul.com>
2025-03-31 16:48:54 +0100

    build: import assets

--- scripts/build-database.ts
+++ scripts/build-database.ts
@@ -1,9 +1,12 @@
 import { Database } from 'bun:sqlite'
-import { mkdir, rm } from 'node:fs/promises'
+import { mkdir, readFile, rm } from 'node:fs/promises'
 import { createRequire } from 'node:module'
+import { basename } from 'node:path'
+import { Glob } from 'bun'
 import { consola } from 'consola'
 import { drizzle } from 'drizzle-orm/bun-sqlite'
-import * as schema from '#~/src/schema'
+import { parse } from 'smol-toml'
+import * as schema from '#~/schema'
 const require = createRequire(import.meta.url)
 
 type DrizzleKitApi = typeof import('drizzle-kit/api')
@@ -36,4 +39,29 @@ for (const stmt of createStatements) {
   db.run(stmt)
 }
 
-consola.success('Database initialized')
+consola.success('Database initialized\n')
+
+consola.start('Importing assets')
+
+import type { Asset } from '../collections/$schemas/.types/assets'
+
+const scan = (path: string) => new Glob(path).scan()
+
+const statements = []
+
+for await (const filePath of scan('./collections/assets/**/*.toml')) {
+  const id = basename(filePath, '.toml')
+  const data = parse(await readFile(filePath, 'utf-8')) as unknown as Asset
+
+  statements.push(
+    db.insert(schema.assets).values({
+      id,
+      name: data.name,
+      reference: data.reference,
+    }),
+  )
+}
+
+await Promise.all(statements)
+
+consola.success('Imported assets\n')
